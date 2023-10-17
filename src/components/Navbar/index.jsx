58f7de6228452dcd15616ce988ef8099a9ff32bd
Eric Marques <eric.marquesgomesmarques@gmail.com>
2023-10-17 17:46:02 -0300

    Adicionando uma navbar ao projeto

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import coffee from '../../assets/img/coffee.png'
+import '../Navbar/style.css'
+
+function Navbar(){
+    return (
+        <div className="navbarhead">
+            <img className="logo" src={coffee} alt="logo de café" />
+            <h2><span>Coffe</span>Jobs</h2>
+            <a href=""><h3 className="jobs">Jobs</h3></a>    
+            <a href=""><h3>Mensagens</h3></a>
+            <a href=""><h3>Perfil</h3></a>
+        </div>
+    )
+}
+
+
+export default Navbar
